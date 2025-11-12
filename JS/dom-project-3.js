0b90f3b3803c1862fd909a155e111a5d7eaa1d85
Abdullahi Muhammed <ma5veconcept@gmail.com>
2025-11-12 08:21:33 -0800

    learning even more about js

--- JS/dom-project-3.js
+++ JS/dom-project-3.js
@@ -1,18 +1,41 @@
- function calculateCost() {
-        const totalCostElement = document.getElementById('total-Cost');
-        const totalCostValue = totalCostElement.value;
-        const number = parseFloat(totalCostValue);
+//  function calculateCost() {
+//         const totalCostElement = document.getElementById('total-Cost');
+//         const totalCostValue = totalCostElement.value;
+//         const number = parseFloat(totalCostValue);
         
-        console.log(totalCostValue);
-        console.log(typeof number);
+//         console.log(totalCostValue);
+//         console.log(typeof number);
 
-        if (totalCostValue < 40) {
+//         if (totalCostValue < 40) {
           
-          document.querySelector('.js-displayCost').innerHTML =`$${totalCostValue}`;
-        } else if (totalCostValue >= 40) {
-          const plusShipping = number + 10;
-          document.querySelector('.js-displayCost').innerHTML =`$${plusShipping}`;
+//           document.querySelector('.js-displayCost').innerHTML =`$${totalCostValue}`;
+//         } else if (totalCostValue >= 40) {
+//           const plusShipping = number + 10;
+//           document.querySelector('.js-displayCost').innerHTML =`$${plusShipping}`;
+
+//         };
+        
+//       };
+
+
+       function calculateTotalCost() {
+        const inputElemet = document.querySelector('.js-totalCost');
+        let cost = Number(inputElemet.value);
+        console.log(cost);
+
+        if (cost >= 40) {
+          cost += 10;
+        } else{
+          cost
+        };
+
+        document.querySelector('.js-displayCost').innerHTML = `$${cost}`
+
+       };
+
+       function calculateWithKey (event) {
+       if (event.key === 'Enter') {
+        calculateTotalCost();
 
         }
-        
       };
