2c1ffab3b1311452b5911201109a929b18db4f5a
Abdullahi Muhammed <ma5veconcept@gmail.com>
2025-11-14 14:48:53 -0800

    need to take time and understand loop and arrays

--- JS/arrays-and-loop.js
+++ JS/arrays-and-loop.js
@@ -15,3 +15,16 @@ myArray.push(100); // .push adds a number to the end of an array.
 myArray.splice(0,2); // .splice removes value from an array. the first number inside the bracket is the index or position of the value we want to remove and the second number inside the bracket is the number of values we want to remove i.e how many values we want to remove.
 console.log(myArray);
 
+
+let i = 1;
+
+while (i <= 5) {
+  console.log(i);
+  i++;
+};
+
+
+for (let i = 1; i <=5; i++) {
+  console.log(i);
+} // for loop is a shorter version of while loop it works the same way.
+
